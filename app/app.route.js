118c32a7787f3398da3eb5a7942375fae462501a
Hoang-Minh Nguyen <alucard522@hotmail.com>
2014-11-24 22:00:45 -0600

    Update to routing mechanism

--- app/app.route.js
+++ app/app.route.js
@@ -1,34 +1,34 @@
-﻿//setup routes to start w/ the `/` symbol
-//m.route.mode = "hash";
-m.route.mode = "pathname";
+﻿m.route.mode = "hash";
 
+//setup routes to start w/ the `/` symbol
 m.route($rootContainer, "/", {
     "/": app,
+    "/home": app,
     "/profile": app,
     "/todo": todo
 });
 
 ///////////////////////////////
 //a sample module
-var dashboard = {
-    controller: function () {
-        this.id = m.route.param("userID");
-    },
-    view: function (controller) {
-        return m("div", controller.id);
-    }
-}
+//var dashboard = {
+//    controller: function () {
+//        this.id = m.route.param("userID");
+//    },
+//    view: function (controller) {
+//        return m("div", controller.id);
+//    }
+//}
 
-var profile = {
-    controller: function () {
-        this.id = m.route.param("userID");
-    },
-    view: function (controller) {
-        return m("div");
-    }
-}
+//var profile = {
+//    controller: function () {
+//        this.id = m.route.param("userID");
+//    },
+//    view: function (controller) {
+//        return m("div");
+//    }
+//}
 
 //define a route
-m.route($rootContainer, "/profile/johndoe", {
-    "/profile/:userID": dashboard
-});
+//m.route($rootContainer, "/profile/johndoe", {
+//    "/profile/:userID": dashboard
+//});
---
+++ app/modules/menu.module.js
@@ -0,0 +1,27 @@
+﻿var menu = {};
+
+menu.Menu = function (data) {
+};
+
+menu.controller = function () {
+};
+
+menu.view = function () {
+    return [
+	m("li.active", [m("a[data-toggle='tab'][href='/home'][title='Go to Home. (H)']", { config: m.route }, "Home")]),
+	m("li", [m("a[data-toggle='tab'][href='#profile'][id='Profile'][title='Go to Profile. (P)']", "Profile")]),
+	m("li.disabled[id='Disabled'][title='Go to Disabled. (P)']", [m("a", "Disabled")]),
+	m("li.dropdown", [
+		m("a.dropdown-toggle[data-toggle='dropdown'][href='javascript:void(0)'][id='Dropdown'][title='Go to Dropdown. (D)']", [
+			"\n                Dropdown ",
+			m("span.caret")
+		]),
+		m("ul.dropdown-menu", [
+			m("li", [m("a[data-toggle='tab'][href='#dropdown1']", "Action")]),
+			m("li.divider"),
+			m("li", [m("a[data-toggle='tab'][href='#dropdown2']", "Another action")])
+		])
+	]),
+	m("li", [m("a[data-toggle='tab'][href='/todo']", {config: m.route}, "Todo App")])
+    ];
+}
