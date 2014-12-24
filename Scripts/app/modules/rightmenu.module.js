bf25134fc1b460ae968820fd570d2ab7c6d623c6
Hoang-Minh Nguyen <alucard522@hotmail.com>
2014-12-23 23:43:05 -0600

    Fixed Right Side Menu, Mithrilfied

--- Scripts/app/modules/rightmenu.module.js
+++ Scripts/app/modules/rightmenu.module.js
@@ -1,5 +1,7 @@
 ﻿var m = require('../../mithril.js');
 var rightmenu = {}
+var Layout = require('../../layout.js');
+layout = new Layout();
 
 rightmenu.rightmenu = function (data) {
 }
@@ -9,9 +11,12 @@ rightmenu.controller = function () {
 
 rightmenu.view = function () {
     return [
-		m("h3", "Menu"),
-		m("a[href='#']", "Register"),
-		m("a[href='#']", "Administration")
+        m("nav.cbp-spmenu.cbp-spmenu-vertical.cbp-spmenu-right[id='right-menu-body']", [
+		    m("h3", "Menu"),
+		    m("a[href='#']", "Register"),
+		    m("a[href='#']", "Administration")
+        ]),
+        m("div.fa.fa-cog.fa-2x.right-menu-button[id='right-menu-button']", { onclick: function () { layout.menuRightButtonClick(this, 'right-menu-body'); } }, "")
     ];
 }
 
--- Scripts/layout.js
+++ Scripts/layout.js
@@ -1,67 +1,63 @@
 ﻿//  This function is trigger the Right SideMenu
-var layouts = function () {
-    //Properties
-    this.menuRight = document.getElementById('right-menu');
-    this.menuRightButton = document.getElementById('right-menu-button');
-    this.hasClass, this.addClass, this.removeClass;
+(function (window) {
+    'use strict';
 
-    this.menuRightButton.onclick = function () {
-        debugger;
-        alert('hi');
-        right_menu.toggle(this, 'active');
-        right_menu.toggle(menuRight, 'cbp-spmenu-open');
+    function classReg(className) {
+        return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+    }
+
+    var hasClass, addClass, removeClass;
+
+    if ('classList' in document.documentElement) {
+        hasClass = function (elem, c) {
+            return elem.classList.contains(c);
+        };
+        addClass = function (elem, c) {
+            elem.classList.add(c);
+        };
+        removeClass = function (elem, c) {
+            elem.classList.remove(c);
+        };
+    }
+    else {
+        hasClass = function (elem, c) {
+            return classReg(c).test(elem.className);
+        };
+        addClass = function (elem, c) {
+            if (!hasClass(elem, c)) {
+                elem.className = elem.className + ' ' + c;
+            }
+        };
+        removeClass = function (elem, c) {
+            elem.className = elem.className.replace(classReg(c), ' ');
+        };
+    }
+
+    function toggleClass(elem, c) {
+        var fn = hasClass(elem, c) ? removeClass : addClass;
+        fn(elem, c);
+    }
+
+    window.right_menu = {
+        // full names
+        hasClass: hasClass,
+        addClass: addClass,
+        removeClass: removeClass,
+        toggleClass: toggleClass,
+        // short names
+        has: hasClass,
+        add: addClass,
+        remove: removeClass,
+        toggle: toggleClass
+    };
+})(window);
+
+var layout = function () {
+    this.menuRightButtonClick = function (menuRightButton, menuRight) {        
+        right_menu.toggle(menuRightButton, 'active');
+        right_menu.toggle(document.getElementById(menuRight), 'cbp-spmenu-open');
         right_menu.toggle(menuRightButton, 'right-menu-button-open');
     };
-
-    ////Initializer
-    //if ('classList' in document.documentElement) {
-    //    this.hasClass = function (elem, c) {
-    //        return elem.classList.contains(c);
-    //    };
-    //    this.addClass = function (elem, c) {
-    //        elem.classList.add(c);
-    //    };
-    //    this.removeClass = function (elem, c) {
-    //        elem.classList.remove(c);
-    //    };
-    //}
-    //else {
-    //    this.hasClass = function (elem, c) {
-    //        return classReg(c).test(elem.className);
-    //    };
-    //    this.addClass = function (elem, c) {
-    //        if (!hasClass(elem, c)) {
-    //            elem.className = elem.className + ' ' + c;
-    //        }
-    //    };
-    //    this.removeClass = function (elem, c) {
-    //        elem.className = elem.className.replace(classReg(c), ' ');
-    //    };
-    //}
-
-    ////Methods
-    //function toggleClass(elem, c) {
-    //    var fn = hasClass(elem, c) ? removeClass : addClass;
-    //    fn(elem, c);
-    //}
-
-    //function classReg(className) {
-    //    return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-    //}
-
-    ////Child Object
-    //this.right_menu = {
-    //    // full names
-    //    hasClass: this.hasClass,
-    //    addClass: this.addClass,
-    //    removeClass: this.removeClass,
-    //    toggleClass: this.toggleClass,
-    //    // short names
-    //    has: this.hasClass,
-    //    add: this.addClass,
-    //    remove: this.removeClass,
-    //    toggle: this.toggleClass
-    //};
 }
 
-module.exports = layouts;
+module.exports = layout;
